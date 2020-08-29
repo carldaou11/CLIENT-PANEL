542040989ceb7e034b1a9cc56d746cd478979d65
carldaou11 <67557435+carldaou11@users.noreply.github.com>
2020-08-29 14:21:20 +0300

    Add files via upload

---
+++ build/precache-manifest.af20f255fc8c9caf08014097c02f7dea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4fbf5ed82b6d12df65f5d4777435f0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e109c34794944a598b29",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c97ebd3cd20371d0ec8a",
+    "url": "/static/js/2.29876277.chunk.js"
+  },
+  {
+    "revision": "329e794e6c358b81ec57b57313449cc4",
+    "url": "/static/js/2.29876277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e109c34794944a598b29",
+    "url": "/static/js/main.1c0fb7a7.chunk.js"
+  },
+  {
+    "revision": "21b7a31307c2b37318a8",
+    "url": "/static/js/runtime-main.bf897520.js"
+  }
+]);
